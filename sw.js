5aff6077871f3b5fbe22f216b2268a29f357b87e
Harr3x <2002henrik@gmail.com>
2026-05-29 13:12:23 +0200

    feat: service worker and PWA manifest for offline support

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'sys-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './app.js',
+  './styles.css',
+  './db.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './utils/golf.js',
+  './components/nav-bar.js',
+  './components/score-cell.js',
+  './screens/home.js',
+  './screens/play.js',
+  './screens/scorecard.js',
+  './screens/stats.js',
+  './screens/courses.js',
+  './screens/players.js',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached ?? fetch(e.request))
+  );
+});
