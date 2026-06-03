36a6f44f63c9adb7fa88f61ae3ba191509596e59
Harr3x <2002henrik@gmail.com>
2026-06-03 11:03:11 +0200

    feat: add VAPID public key to config

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 export const SUPABASE_URL = 'https://zrsubcqhwyutyvphdwsb.supabase.co';
 export const SUPABASE_ANON_KEY = 'sb_publishable_dcpXZJeSo3l1H_w_28pw2w_e17cNRex';
+export const VAPID_PUBLIC_KEY = 'BNub5iobsCDAAucSk04BitHms_kSVes4APQHUiidrzTv9WnBjsTJaUULqwU3bJPgrnedWNO8Zl7U-iiFUOsQs0E';
